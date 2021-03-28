380a2156de9ce21eb32dfcfd3ee9395351460cdf
Pierre Talbot <ptalbot@hyc.io>
2021-03-29 00:05:59 +0200

    Demonstration of testing with JUnit.

--- src/main/java/lab2/DynamicArray.java
+++ src/main/java/lab2/DynamicArray.java
@@ -11,28 +11,36 @@ public class DynamicArray {
 
   // Returns `true` if the array is empty.
   public boolean isEmpty() {
-    return true;
+    return size == 0;
   }
 
   // Returns the size of the array.
   public int size() {
-    return 0;
+    return size;
   }
 
   // Remove all elements from data.
   public void clear() {
+    size = 0;
   }
 
   // Create a `String` with the elements of the array separated by comma, without a new line character at the end.
   // For instance: 4, 5, 6
   public String toString() {
-    return "";
+    String s = "";
+    for(int i = 0; i < size - 1; ++i) {
+      s += data[i] + ", ";
+    }
+    if(!isEmpty()) {
+      s += data[size-1];
+    }
+    return s;
   }
 
   // Returns `true` if the array `data` is full: no more element can be added to `data`.
   // Returns `false` otherwise.
   private boolean isFull() {
-    return true;
+    return size == data.length;
   }
 
   // If the array `data` is full:
@@ -40,11 +48,21 @@ public class DynamicArray {
   //  2. Copy the elements of `data` into `data2`.
   //  3. Assign `data2` to `data`.
   private void realloc() {
+    if(isFull()) {
+      int[] data2 = new int[data.length * 2];
+      for(int i = 0; i < data.length; ++i) {
+        data2[i] = data[i];
+      }
+      data = data2;
+    }
   }
 
   // The element `x` is added to `data`, and `size` is incremented by one.
   // `data` is automatically resized if it is too small.
   public void add(int x) {
+    realloc();
+    data[size] = x;
+    size += 1;
   }
 
   private void checkIndex(int idx) {
@@ -55,15 +73,23 @@ public class DynamicArray {
 
   // Set the ith element of `data` to `x`.
   public void set(int idx, int x) {
+    checkIndex(idx);
+    data[idx] = x;
   }
 
   // Return the element at the index `idx` of `data`.
   public int get(int idx) {
-    return 0;
+    checkIndex(idx);
+    return data[idx];
   }
 
   // Remove the element at index `idx`.
   // Shift all the elements after `idx` of one position to the left.
   public void remove(int idx) {
+    checkIndex(idx);
+    for(int i = idx; i < size - 1; ++i) {
+      data[i] = data[i + 1];
+    }
+    size -= 1;
   }
 }
--- src/main/java/lab2/pcg/Pokedeck.java
+++
@@ -1,7 +0,0 @@
-package lab2.pcg;
-
-public class Pokedeck {
-  public static void main(String[] args) {
-    System.out.println("Welcome to my Pokedeck app!");
-  }
-}
